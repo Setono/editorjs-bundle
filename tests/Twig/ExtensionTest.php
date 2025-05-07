f026e4b6bfe3353530ec817c250ac3a69ad086eb
Joachim Løvgaard <joachim@loevgaard.dk>
2025-05-07 14:54:23 +0200

    Add small twig test

---
+++ tests/Twig/ExtensionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\EditorJSBundle\Tests\Twig;
+
+use Prophecy\PhpUnit\ProphecyTrait;
+use Setono\EditorJS\Parser\ParserInterface;
+use Setono\EditorJS\Renderer\RendererInterface;
+use Setono\EditorJSBundle\Twig\Extension;
+use Setono\EditorJSBundle\Twig\Runtime;
+use Twig\RuntimeLoader\RuntimeLoaderInterface;
+use Twig\Test\IntegrationTestCase;
+use Webmozart\Assert\Assert;
+
+final class ExtensionTest extends IntegrationTestCase
+{
+    use ProphecyTrait;
+
+    public function getRuntimeLoaders(): array
+    {
+        $parser = $this->prophesize(ParserInterface::class);
+        $renderer = $this->prophesize(RendererInterface::class);
+
+        $runtimeLoader = new class($parser->reveal(), $renderer->reveal()) implements RuntimeLoaderInterface {
+            public function __construct(
+                private readonly ParserInterface $parser,
+                private readonly RendererInterface $renderer,
+            ) {
+            }
+
+            /**
+             * @param string $class
+             */
+            public function load($class): Runtime
+            {
+                Assert::same($class, Runtime::class);
+
+                return new Runtime($this->parser, $this->renderer);
+            }
+        };
+
+        return [$runtimeLoader];
+    }
+
+    public function getExtensions(): array
+    {
+        return [
+            new Extension(),
+        ];
+    }
+
+    protected function getFixturesDir(): string
+    {
+        return __DIR__ . '/Fixtures/';
+    }
+}
