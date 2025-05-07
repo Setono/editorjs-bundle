8763eefb29f43123d958e36db62e1ec3399ea525
Joachim Løvgaard <joachim@loevgaard.dk>
2025-05-07 14:06:59 +0200

    Fix deps

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('symfony/twig-bundle', [ErrorType::UNUSED_DEPENDENCY]) // Used for creating the 'twig' service
 ;
--- ecs.php
+++ ecs.php
@@ -7,6 +7,8 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 return static function (ECSConfig $config): void {
     $config->import('vendor/sylius-labs/coding-standard/ecs.php');
     $config->paths([
-        'src', 'tests'
+        'src',
+        'tests',
+        'composer-dependency-analyser.php',
     ]);
 };
